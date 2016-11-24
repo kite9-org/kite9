9b5e29828265592d5f9c74d00688f0f29246cdac
Rob Moffat <robmoffat@mac.com>
2016-11-24 15:45:58 +0000

    Removed unnecessary code frrom CSO Step 6-34/456

--- src/main/java/org/kite9/diagram/visualization/compaction/position/optstep/ContainerSizeOptimisationStep.java
+++ src/main/java/org/kite9/diagram/visualization/compaction/position/optstep/ContainerSizeOptimisationStep.java
@@ -2,25 +2,18 @@ package org.kite9.diagram.visualization.compaction.position.optstep;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.kite9.diagram.adl.Container;
 import org.kite9.diagram.adl.DiagramElement;
-import org.kite9.diagram.common.algorithms.det.UnorderedSet;
 import org.kite9.diagram.common.algorithms.so.OptimisationStep;
 import org.kite9.diagram.common.algorithms.so.Slideable;
-import org.kite9.diagram.common.elements.Edge;
-import org.kite9.diagram.common.elements.PositionAction;
-import org.kite9.diagram.common.elements.Vertex;
 import org.kite9.diagram.common.objects.Pair;
 import org.kite9.diagram.position.Direction;
 import org.kite9.diagram.visualization.compaction.Compaction;
 import org.kite9.diagram.visualization.compaction.Segment;
 import org.kite9.diagram.visualization.compaction.position.SegmentSlackOptimisation;
-import org.kite9.diagram.visualization.orthogonalization.Dart;
 import org.kite9.framework.logging.Logable;
 
 /**
@@ -77,55 +70,10 @@ public class ContainerSizeOptimisationStep implements OptimisationStep, Logable 
 
 	}
 	
-	/**
-	 * This works out for a particular segment, which side of the underlying diagram element it represents.
-	 * This works by tracing darts with the same underlying, and looking at which direction they go in.
-	 */
 	private Direction getSegmentUnderlyingSide(Segment s, DiagramElement underlying) {
-		Set<Direction> sides = new LinkedHashSet<Direction>();
-		Set<Direction> planeDirection = new LinkedHashSet<Direction>(4);
-		if (s.getDimension()==PositionAction.XAction) {
-			planeDirection.add(Direction.LEFT);
-			planeDirection.add(Direction.RIGHT);
-		} else {
-			planeDirection.add(Direction.UP);
-			planeDirection.add(Direction.DOWN);
-		}
-		
-		for (Direction direction : planeDirection) {
-			boolean yes = checkDiagramElementContinues(s, underlying, direction);
-			if (!yes) {
-				sides.add(direction);
-			}
-		}
-		
-		if (sides.size()==1) {
-			return sides.iterator().next();
-		}
-		
-		return null;
-		
+		return s.getUnderlyingSide();
 	}
 
-	private boolean checkDiagramElementContinues(Segment s, DiagramElement underlying, Direction direction) {
-		for (Vertex v : s.getVerticesInSegment()) {
-			for (Edge e : v.getEdges()) {
-				if (e instanceof Dart) {
-					DiagramElement dartUnderlying = ((Dart) e).getOriginalUnderlying();
-					if (dartUnderlying==underlying) {
-						Direction eDir = e.getDrawDirectionFrom(v);
-						if (eDir==direction) {
-							return true;
-						}
-					}
-				}
-			}
-		}
-		
-		return false;
-	}
-
-	
 	private void maximizeDistance(SegmentSlackOptimisation xo, Slideable from, Slideable to) {
 		int slackAvailable = to.getMaximumPosition() - from.getMinimumPosition();
 		from.decreaseMaximum(from.getMinimumPosition());
