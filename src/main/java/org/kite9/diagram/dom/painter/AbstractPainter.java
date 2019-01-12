75015cc4812e490b58e0718ce797130340861a89
Rob Moffat <robmoffat@mac.com>
2019-01-12 21:04:32 +0000

    New rules about attributes on copied nodes

--- src/main/java/org/kite9/diagram/dom/painter/AbstractPainter.java
+++ src/main/java/org/kite9/diagram/dom/painter/AbstractPainter.java
@@ -11,7 +11,9 @@ import org.kite9.diagram.model.SizedRectangular;
 import org.kite9.diagram.model.position.Layout;
 import org.kite9.diagram.model.position.RectangleRenderingInformation;
 import org.kite9.framework.common.Kite9ProcessingException;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.css.Rect;
 
 /**
@@ -40,13 +42,30 @@ public abstract class AbstractPainter implements Painter {
 	}
 
 	protected void addAttributes(StyledKite9SVGElement toUse, Element out) {
+		copyAttributes(toUse, out);
+		
 		String id = r.getID();
 		if (id.length() > 0) {
 			out.setAttribute("id", id);
 		}
+		
 		out.setAttribute("kite9-elem", toUse.getTagName());
+		out.setAttribute("class", toUse.getAttribute("class")+" kite9-"+toUse.getTagName());
+
 		addDebugAttributes(out);
 	}
+	
+	public void copyAttributes(Element from, Element to) {
+        NamedNodeMap attributes = from.getAttributes();
+        for (int i = 0; i < attributes.getLength(); i++)
+        {
+            Attr node = (Attr) attributes.item(i);
+            
+            Attr copy = to.getOwnerDocument().createAttribute(node.getName());
+            copy.setNodeValue(node.getNodeValue());
+            to.setAttributeNode(copy);
+        }
+    }
 
 	private void addDebugAttributes(Element out) {
 		StringBuilder debug = new StringBuilder();
