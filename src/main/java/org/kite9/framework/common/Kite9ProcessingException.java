b6ac03b2b02210f4da7920d7b287267fcae54fbf
Rob Moffat <robmoffat@mac.com>
2020-09-29 19:46:30 +0100

    Prevented chaining Kite9ProcessingException

--- src/main/java/org/kite9/framework/common/Kite9ProcessingException.java
+++ src/main/java/org/kite9/framework/common/Kite9ProcessingException.java
@@ -10,18 +10,27 @@ package org.kite9.framework.common;
  */
 public class Kite9ProcessingException extends RuntimeException {
 
-    private static final long serialVersionUID = -1338453592786354494L;
+    /**
+     * These prevent us from creating traces with multiple {@link Kite9ProcessingException}s in them.
+     */
+    private static String correctMessage(String arg0, Throwable arg1) {
+		return arg1 instanceof Kite9ProcessingException ? arg0 + "\n"+arg1.getMessage() : arg0;
+	}
 
+	protected static Throwable correctThrowable(Throwable arg1) {
+		return arg1 instanceof Kite9ProcessingException ? arg1.getCause() : arg1;
+	}
+    
     public Kite9ProcessingException(String arg0, Throwable arg1) {
-	super(arg0, arg1);
+    	super(correctMessage(arg0, arg1), correctThrowable(arg1));
     }
 
     public Kite9ProcessingException(String arg0) {
-	super(arg0);
+    	super(arg0);
     }
 
     public Kite9ProcessingException(Throwable arg0) {
-	super(arg0);
+    	super(correctThrowable(arg0));
     }
     
 }
