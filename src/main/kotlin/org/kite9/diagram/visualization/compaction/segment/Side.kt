3c9b0365274ba0c37cd5b09a8ed0b8f8671e24ae
Rob Moffat <rob@kite9.com>
2021-01-09 13:45:10 +0000

    Converting interfaces

---
+++ src/main/kotlin/org/kite9/diagram/visualization/compaction/segment/Side.kt
@@ -0,0 +1,5 @@
+package org.kite9.diagram.visualization.compaction.segment
+
+enum class Side {
+    START, END, NEITHER, BOTH
+}
---
+++ src/main/kotlin/org/kite9/diagram/visualization/compaction/segment/UnderlyingInfo.kt
@@ -0,0 +1,5 @@
+package org.kite9.diagram.visualization.compaction.segment
+
+import org.kite9.diagram.model.DiagramElement
+
+data class UnderlyingInfo(val diagramElement: DiagramElement, val side: Side)
