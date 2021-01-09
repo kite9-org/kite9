69bde184e32785717bdec4323d95be1961d1e4b6
Rob Moffat <rob@kite9.com>
2021-01-09 13:26:04 +0000

    Converting interfaces

---
+++ src/main/kotlin/org/kite9/diagram/visualization/compaction/Embedding.kt
@@ -0,0 +1,21 @@
+package org.kite9.diagram.visualization.compaction
+
+import org.kite9.diagram.visualization.compaction.segment.Segment
+import org.kite9.diagram.visualization.orthogonalization.DartFace
+
+/**
+ * Embeddings are basically wrappers around outer faces in the diagram.  We compact outer faces from the
+ * bottom up.
+ *
+ * @author robmoffat
+ */
+interface Embedding {
+    /**
+     * Used in rectangularization
+     */
+    val dartFaces: List<DartFace>
+    val innerEmbeddings: List<Embedding>
+    fun getVerticalSegments(c: Compaction): Collection<Segment>
+    fun getHorizontalSegments(c: Compaction): Collection<Segment>
+    val isTopEmbedding: Boolean
+}
