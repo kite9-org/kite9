10a9db9e51a02bdc9810a7e5e74565a7b8ba60a7
Rob Moffat <rob@kite9.com>
2021-01-21 17:15:02 +0000

    Orth

--- src/main/kotlin/org/kite9/diagram/visualization/orthogonalization/flow/face/SplitLocation.kt
+++ src/main/kotlin/org/kite9/diagram/visualization/orthogonalization/flow/face/SplitLocation.kt
@@ -3,5 +3,5 @@ package org.kite9.diagram.visualization.orthogonalization.flow.face
 import org.kite9.diagram.common.algorithms.fg.Node
 
 internal data class SplitLocation(val split: Node, val location: Any) {
-    
+
 }
