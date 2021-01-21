92121740cfee3077878900aec6c063e3938847b7
Rob Moffat <rob@kite9.com>
2021-01-21 11:17:33 +0000

    Enums

--- src/main/kotlin/org/kite9/diagram/visualization/planarization/mgt/router/CrossingType.java
+++
@@ -1,6 +0,0 @@
-package org.kite9.diagram.visualization.planarization.mgt.router;
-
-/**
- * Describes how we insert the edge, wrt to the direction the edge has been given.
- */
-public enum CrossingType { STRICT, NOT_BACKWARDS, UNDIRECTED }
