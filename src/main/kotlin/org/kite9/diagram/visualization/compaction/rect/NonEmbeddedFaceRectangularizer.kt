52e5c734e1203a1fa46eb1d2aa7881cbad98fa88
Rob Moffat <rob@kite9.com>
2021-01-29 13:36:41 +0000

    Compaction

---
+++ src/main/kotlin/org/kite9/diagram/visualization/compaction/rect/NonEmbeddedFaceRectangularizer.kt
@@ -0,0 +1,24 @@
+package org.kite9.diagram.visualization.compaction.rect
+
+import org.kite9.diagram.visualization.compaction.Compaction
+import org.kite9.diagram.visualization.compaction.Compactor
+import org.kite9.diagram.visualization.compaction.Embedding
+import org.kite9.diagram.visualization.display.CompleteDisplayer
+import org.kite9.diagram.visualization.orthogonalization.DartFace
+import java.util.stream.Collectors
+
+open class NonEmbeddedFaceRectangularizer(cd: CompleteDisplayer?) : MidSideCheckingRectangularizer(cd) {
+
+    override fun compact(c: Compaction, r: Embedding, rc: Compactor) {
+        log.send("NonEmbeddedFaceRectangularizer: $r")
+        super.compact(c, r, rc)
+    }
+
+    override fun selectFacesToRectangularize(c: Compaction, faces: List<DartFace>): List<DartFace> {
+        return faces
+            .filter {
+                val r = c.getFaceSpace(it)
+                r == null
+            }
+    }
+}
