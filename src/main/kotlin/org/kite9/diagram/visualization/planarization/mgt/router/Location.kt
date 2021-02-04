f319ef95730c10993fa89ad18a280c85c1587d3e
Rob Moffat <rob@kite9.com>
2021-02-04 08:54:34 +0000

    Javascript conversion - single page of errors

--- src/main/kotlin/org/kite9/diagram/visualization/planarization/mgt/router/Location.kt
+++ src/main/kotlin/org/kite9/diagram/visualization/planarization/mgt/router/Location.kt
@@ -5,26 +5,10 @@ import org.kite9.diagram.common.elements.vertex.Vertex
 /**
  * This class holds the location of the ssp node, which can be either outsideEdge or below or arriving at any given vertex.
  */
-class Location(var p: AbstractRouteFinder.Place?, val trailEndVertex: Int, val vertex: Vertex) {
+data class Location(val p: AbstractRouteFinder.Place?, val trailEndVertex: Int, val vertex: Vertex) {
 
     override fun toString(): String {
         return "LOC[p=$p, vertex=$trailEndVertex,v=$trailEndVertex]"
     }
 
-    override fun hashCode(): Int {
-        val prime = 31
-        var result = 1
-        result = prime * result + if (p == null) 0 else p.hashCode()
-        result = prime * result + trailEndVertex
-        return result
-    }
-
-    override fun equals(obj: Any?): Boolean {
-        if (this === obj) return true
-        if (obj == null) return false
-        if (javaClass != obj.javaClass) return false
-        val other = obj as Location
-        if (p != other.p) return false
-        return if (trailEndVertex != other.trailEndVertex) false else true
-    }
 }
