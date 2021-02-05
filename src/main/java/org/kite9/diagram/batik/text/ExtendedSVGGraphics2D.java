42a54c6548329279e3b06ed80a862449efb1f4fb
Rob Moffat <rob@kite9.com>
2021-02-05 17:14:29 +0000

    Fixed issue in ESVG

--- src/main/java/org/kite9/diagram/batik/text/ExtendedSVGGraphics2D.java
+++ src/main/java/org/kite9/diagram/batik/text/ExtendedSVGGraphics2D.java
@@ -13,14 +13,9 @@ import org.apache.batik.gvt.font.GVTFontFace;
 import org.apache.batik.gvt.font.GVTFontFamily;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.util.ParsedURL;
-<<<<<<< HEAD
-import org.kite9.framework.logging.Logable;
-import org.kite9.framework.logging.LogicException;
-=======
 import org.kite9.diagram.logging.Kite9Log;
 import org.kite9.diagram.logging.Logable;
 import org.kite9.diagram.logging.LogicException;
->>>>>>> f28453fc4df71540e3664fe3582b68b6594dabbb
 import org.w3c.dom.Element;
 
 
