524522d0667b29b34d42e4a20301a6fbefd24404
Rob Moffat <rob@kite9.com>
2021-02-10 17:43:32 +0000

    DOM Model

---
+++ src/main/kotlin/org/kite9/diagram/dom/model/DecalLeafImpl.kt
@@ -0,0 +1,26 @@
+package org.kite9.diagram.dom.model
+
+import org.kite9.diagram.dom.bridge.ElementContext
+import org.kite9.diagram.dom.painter.LeafPainter
+import org.kite9.diagram.model.Decal
+import org.kite9.diagram.model.DiagramElement
+import org.kite9.diagram.model.Leaf
+import org.kite9.diagram.model.position.RenderingInformation
+import org.kite9.diagram.model.style.ContentTransform
+import org.w3c.dom.Element
+
+class DecalLeafImpl(
+    el: Element,
+    parent: DiagramElement,
+    ctx: ElementContext,
+    lo: LeafPainter,
+    t: ContentTransform
+) : AbstractModelDiagramElement(
+    el, parent, ctx, lo, t
+), Decal, Leaf {
+
+    override fun getRenderingInformation(): RenderingInformation {
+        return getParent()!!.getRenderingInformation()
+    }
+
+}
