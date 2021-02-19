fac44d843bf9a04adac207071366f2e9131690f0
Rob Moffat <rob@kite9.com>
2021-02-19 18:35:35 +0000

    Separate JS Compile

--- src/main/kotlin/org/kite9/diagram/dom/model/TemporaryConnectedImpl.kt
+++ src/main/kotlin/org/kite9/diagram/dom/model/TemporaryConnectedImpl.kt
@@ -80,9 +80,9 @@ class TemporaryConnectedImpl(parent: DiagramElement, id: String, val painter: Pa
         return NONE
     }
 
-    override fun output(d: Document, p: XMLProcessor): Element {
+    override fun output(d: Document, p: XMLProcessor): Element? {
         painter.setDiagramElement(this)
-        return painter.output(d, p)!!
+        return painter.output(d, p)
     }
 
     override fun getMargin(d: Direction): Double {
