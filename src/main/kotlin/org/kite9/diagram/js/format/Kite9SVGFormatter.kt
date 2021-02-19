607917734475ba5434d786d36c15087e862c64f0
Rob Moffat <rob@kite9.com>
2021-02-19 09:49:47 +0000

    Attempting to get deploy working in github

---
+++ src/main/kotlin/org/kite9/diagram/js/format/Kite9SVGFormatter.kt
@@ -0,0 +1,16 @@
+package org.kite9.diagram.js.format
+
+import org.w3c.dom.Element
+import kotlin.js.ExperimentalJsExport
+
+/**
+ * Top-level function that can be called from javascript to render
+ * Kite9 SVG properly in-browser
+ */
+@ExperimentalJsExport
+fun formatDOM(e: Element) {
+
+    println("hello")
+
+
+}
