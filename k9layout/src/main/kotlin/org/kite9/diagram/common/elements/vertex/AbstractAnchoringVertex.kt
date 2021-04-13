c4be8b4128c0aeefa8180059eb20f5468b4ade17
Rob Moffat <rob@kite9.com>
2021-04-13 08:39:39 +0100

    Simplified anchor

--- k9layout/src/main/kotlin/org/kite9/diagram/common/elements/vertex/AbstractAnchoringVertex.kt
+++ k9layout/src/main/kotlin/org/kite9/diagram/common/elements/vertex/AbstractAnchoringVertex.kt
@@ -14,66 +14,10 @@ import org.kite9.diagram.model.position.VPos
  */
 abstract class AbstractAnchoringVertex(id: String) : AbstractVertex(id) {
 
-    class Anchor(val ud: VPos?, val lr: HPos?, val de: DiagramElement) {
-
-        private val rI: RectangleRenderingInformation
-            private get() = de.getRenderingInformation() as RectangleRenderingInformation
-
-        fun setX(x: Double) {
-            try {
-                if (de == null) {
-                    return
-                }
-                val ri = rI
-                val position = ri.position
-                val size = ri.size
-
-                var l: Double = if (position == null) 0.0 else position.x()
-                var r = if (size == null) l else l + size.width()
-                val u: Double = if (position == null) 0.0 else position.y()
-                val d = if (size == null) u else u + size.height()
-
-                if (lr == HPos.LEFT) {
-                    l = x
-                } else if (lr == HPos.RIGHT) {
-                    r = x
-                }
-                ri.position = CostedDimension2D(l, u)
-                ri.size = CostedDimension2D(r - l, d - u)
-            } catch (e: NullPointerException) {
-                throw LogicException("NPE setting position of $this", e)
-            }
-        }
-
-        fun setY(y: Double) {
-            try {
-                if (de == null) {
-                    return
-                }
-                val ri = rI
-                val position = ri.position
-                val size = ri.size
-
-                val l: Double = if (position == null) 0.0 else position.x()
-                val r = if (size == null) l else l + size.width()
-                var u: Double = if (position == null) 0.0 else position.y()
-                var d = if (size == null) u else u + size.height()
-
-                if (ud == VPos.UP) {
-                    u = y
-
-                } else if (ud == VPos.DOWN) {
-                    d = y
-                }
-                ri.position = CostedDimension2D(l, u)
-                ri.size = CostedDimension2D(r - l, d - u)
-            } catch (e: NullPointerException) {
-                throw LogicException("NPE setting position of $this", e)
-            }
-        }
+    data class Anchor(val ud: VPos?, val lr: HPos?, val de: DiagramElement) {
 
         override fun toString(): String {
-            return de.toString() + "-" + lr + "-" + ud
+            return "$de-$lr-$ud"
         }
     }
 }
