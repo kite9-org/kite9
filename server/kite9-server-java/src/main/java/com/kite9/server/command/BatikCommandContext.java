db54acb1e88d45464973da16145e9659ec29126a
Rob Moffat <rob@kite9.com>
2021-09-17 13:13:52 +0100

    fixing build

--- server/kite9-server-java/src/main/java/com/kite9/server/command/BatikCommandContext.java
+++ server/kite9-server-java/src/main/java/com/kite9/server/command/BatikCommandContext.java
@@ -100,7 +100,7 @@ public class BatikCommandContext implements CommandContext {
         String xml = new String(Base64.getDecoder().decode(base64xml), Charsets.UTF_8);
         Document nDoc = (Document) adl.parseDocument(xml, null);
         Element n = nDoc.getDocumentElement();
-        System.out.println("Converting: "+xml+" with id "+n.getAttribute("id")+" into " + new XMLHelper().toXML(n));
+//        System.out.println("Converting: "+xml+" with id "+n.getAttribute("id")+" into " + new XMLHelper().toXML(n));
         return n;
     }
 
