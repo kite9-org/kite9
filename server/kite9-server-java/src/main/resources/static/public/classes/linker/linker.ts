5b0daade85682a555cc4a1d7d58a1c9cb7761611
Rob Moffat <rob@kite9.com>
2022-10-24 09:40:43 +0100

    Converted linker to ts

--- server/kite9-server-java/src/main/resources/static/public/classes/linker/linker.ts
+++ server/kite9-server-java/src/main/resources/static/public/classes/linker/linker.ts
@@ -1,20 +1,28 @@
-import { getSVGCoords, getElementPageBBox, getMainSvg } from '/public/bundles/screen.js' ;
-import { createUniqueId, parseInfo, onlyUnique, changeId } from '/public/bundles/api.js' 
+import { getSVGCoords, getElementPageBBox, getMainSvg } from '../../bundles/screen.js' ;
+import { createUniqueId, onlyUnique, changeId } from '../../bundles/api.js' 
+
+type point = {x : number, y: number}
+type animate = (e: Element, x: point, y: point) => void
+type callback = (l: Linker, e: Event, perform: boolean) => void
+type getLinkTarget = (v: Element) => Element
 
 /**
  * Provides functionality for drawing links on the diagram, and keeping track of them.
  */
 export class Linker {
-	 
-	constructor(animate, getLinkTarget) {
-		this.svg = getMainSvg();
-		this.drawing = [];
+
+	svg = getMainSvg();
+	drawing = [];
+	animate : animate;
+	getLinkTarget : getLinkTarget;
+	callbacks : callback[] = [];
+	mouseCoords : point
+
+	constructor(animate : animate, getLinkTarget: getLinkTarget = undefined) {
 		this.animate = animate;
 		
-		this.callbacks = [];
-		
 		if (getLinkTarget == undefined) {
-			this.getLinkTarget = function(v) {
+			this.getLinkTarget = function(v : Element) {
 				if (v.hasAttribute("k9-ui") && v.hasAttribute("id")) {
 					if (v.getAttribute("k9-ui").includes('connect')) {
 						return v;
@@ -33,12 +41,12 @@ export class Linker {
 		
 	}
 	
-	add(cb) {
+	add(cb : callback) {
 		// nb. additions go to the start
 		this.callbacks.unshift(cb);
 	}
 	
-	start(selectedElements, template) {
+	start(selectedElements : Element[], template: Element) {
 		if (template == undefined) {
 			alert("Link Template Not Loaded");
 			return;
@@ -47,7 +55,7 @@ export class Linker {
 		Array.from(selectedElements)
 			.filter(onlyUnique)
 			.forEach(e => {
-				var newLink = template.cloneNode(true);
+				const newLink = template.cloneNode(true) as Element;
 				this.svg.appendChild(newLink);
 				newLink.setAttribute("temp-from", e.getAttribute("id"));
 				const newId = createUniqueId();
@@ -56,15 +64,16 @@ export class Linker {
 				newLink.removeAttribute("default");
 	
 				const bbox = getElementPageBBox(e)
-				var from = { x: bbox.x + bbox.width/2, y: bbox.y + bbox.height/2 };
+				const from = { x: bbox.x + bbox.width/2, y: bbox.y + bbox.height/2 };
 				this.drawing.push(newLink);
-				newLink.start = from;
+				// not sure this is really allowed, but works in js 
+				(newLink as any).start = from;
 				newLink.setAttributeNS(null, 'pointer-events', 'none');
 				this.animate(newLink, from, this.mouseCoords);
 			});
 	}
 	
-	move(evt) {
+	move(evt : Event) {
 		this.mouseCoords = getSVGCoords(evt);
 		this.drawing.forEach(e => {
 			this.animate(e, e.start, this.mouseCoords);
@@ -72,7 +81,7 @@ export class Linker {
 		
 	}
 	
-	moveCoords(x1, y1, x2, y2) {
+	moveCoords(x1: number, y1: number, x2: number, y2: number) {
 		this.drawing.forEach(e => {
 			this.animate(e, {x: x1, y: y1}, {x: x2, y: y2});
 		});
@@ -85,7 +94,7 @@ export class Linker {
 		this.drawing = [];
 	}
 
-	end(evt, perform=true) {
+	end(evt : Event, perform=true) {
 		if (this.drawing.length == 0) {
 			return;
 		}
