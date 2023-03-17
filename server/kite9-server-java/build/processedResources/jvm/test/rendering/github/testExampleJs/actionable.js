953a4caadf9d348186b3525980a7066237646c73
Rob Moffat <rob@kite9.com>
2023-03-17 12:42:30 +0000

    Fixing CI

---
+++ server/kite9-server-java/build/processedResources/jvm/test/rendering/github/testExampleJs/actionable.js
@@ -0,0 +1,37 @@
+import { getMainSvg } from '/public/bundles/screen.js'
+
+/**
+ * Allows the context menu to appear when the user clicks an element with an id
+ */    
+export function initActionable(contextMenu, selector) {
+
+	/**
+	 * Displays a context menu when the user clicks on an element.
+	 */
+	function click(event) {
+		contextMenu.destroy();
+		
+		if (getMainSvg().style.cursor =='wait') {
+			return;
+		}
+		
+		contextMenu.handle(event);
+		event.stopPropagation();
+	}
+	
+	if (selector == undefined) {
+		selector = function() {
+			return getMainSvg().querySelectorAll("[id]");
+		}
+	}
+
+	window.addEventListener('DOMContentLoaded', function() {
+		
+		selector().forEach(function(v) {
+	    	// set up listeners
+	    	v.removeEventListener("click", click);
+	    	v.addEventListener("click", click);
+	    })
+	});
+
+}
