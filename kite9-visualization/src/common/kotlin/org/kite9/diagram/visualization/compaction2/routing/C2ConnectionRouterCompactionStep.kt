578f1045cbff863a0b74b7e2b78b0ee4fa6eb1bc
Rob Moffat <rob@kite9.com>
2023-11-20 09:40:30 +0000

    Allowed turns

--- kite9-visualization/src/common/kotlin/org/kite9/diagram/visualization/compaction2/routing/C2ConnectionRouterCompactionStep.kt
+++ kite9-visualization/src/common/kotlin/org/kite9/diagram/visualization/compaction2/routing/C2ConnectionRouterCompactionStep.kt
@@ -4,6 +4,7 @@ import org.kite9.diagram.common.algorithms.ssp.NoFurtherPathException
 import org.kite9.diagram.common.elements.Dimension
 import org.kite9.diagram.model.Connected
 import org.kite9.diagram.model.Connection
+import org.kite9.diagram.model.Port
 import org.kite9.diagram.model.position.Direction
 import org.kite9.diagram.visualization.compaction2.*
 import org.kite9.diagram.visualization.display.CompleteDisplayer
@@ -19,6 +20,7 @@ class C2ConnectionRouterCompactionStep(cd: CompleteDisplayer, r: GroupResult) : 
     override val prefix = "C2CR"
     override val isLoggingEnabled = true
 
+    val usedStartEndPoints = mutableSetOf<C2Point>()
 
     private fun createPoints(c2: C2Compaction, d: Connected, arriving: Boolean) : Set<C2Point> {
         val h = c2.getSlackOptimisation(Dimension.H)
@@ -31,24 +33,45 @@ class C2ConnectionRouterCompactionStep(cd: CompleteDisplayer, r: GroupResult) : 
             C2Point(hss.c, vss.r, if (arriving) Direction.UP else Direction.DOWN),  // bottom
             C2Point(vss.c, hss.l, if (arriving) Direction.RIGHT else Direction.LEFT),  // left
             C2Point(vss.c, hss.r, if (arriving) Direction.LEFT else Direction.RIGHT)   // right
-        )
+        ).minus(usedStartEndPoints)
     }
 
     private fun insertLink(c2: C2Compaction, c: Connection) {
         try {
             val startingPoints = createPoints(c2, c.getFrom(), false)
             val endingPoints = createPoints(c2, c.getTo(), true)
+            val allowTurns = (c.getDrawDirection() == null) || (c.getRenderingInformation().isContradicting)
 
-            val doer = C2SlideableSSP(startingPoints, endingPoints, log)
+            val doer = C2SlideableSSP(startingPoints, endingPoints, allowTurns, log)
             log.send("Routing: $c")
             val out = doer.createShortestPath()
 
             log.send("Found shortest path: $out")
 
+            updateUsedStartEndPoints(out.point, c.getTo())
+            updateUsedStartEndPoints(getStart(out), c.getFrom())
+
+
             c2.getRoutes()[c] = out
 
         } catch (e: NoFurtherPathException) {
             log.error("Couldn't route: $c")
+        }
+    }
+
+    private fun getStart(out: C2Route) : C2Point {
+        return if (out.prev != null) {
+            getStart(out.prev)
+        } else {
+            out.point
+        }
+    }
+
+    private fun updateUsedStartEndPoints(out: C2Point, from: Connected) {
+        if (from is Port) {
+            // ports are allowed to have multiple connections
+        } else {
+            usedStartEndPoints.add(out)
         }
     }
 
--- kite9-visualization/src/common/kotlin/org/kite9/diagram/visualization/compaction2/routing/C2SlideableSSP.kt
+++ kite9-visualization/src/common/kotlin/org/kite9/diagram/visualization/compaction2/routing/C2SlideableSSP.kt
@@ -4,10 +4,13 @@ import org.kite9.diagram.common.algorithms.ssp.AbstractSSP
 import org.kite9.diagram.common.algorithms.ssp.State
 import org.kite9.diagram.logging.Kite9Log
 import org.kite9.diagram.model.position.Direction
+import org.kite9.diagram.visualization.compaction2.C2BufferSlideable
+import org.kite9.diagram.visualization.compaction2.C2Slideable
 
 class C2SlideableSSP(
     val start: Set<C2Point>,
     val end: Set<C2Point>,
+    val allowTurns: Boolean,
     val log: Kite9Log
 ) : AbstractSSP<C2Route>() {
     override fun pathComplete(r: C2Route): Boolean {
@@ -15,19 +18,40 @@ class C2SlideableSSP(
     }
 
     override fun generateSuccessivePaths(r: C2Route, s: State<C2Route>) {
-        val d1 = r.point.a  // along
-        val d2 = r.point.b  // arriving at
+        val along = r.point.a  // along
+        val perp = r.point.b  // arriving at
         val d = r.point.d
         log.send("Extending: ${r.cost} -> $r")
 
+        advance(d, perp, r, along, s)
+
+        if (allowTurns) {
+            if ((along is C2BufferSlideable) && (perp is C2BufferSlideable)) {
+                val dc = Direction.rotateClockwise(d)
+                advance(dc, along, r, perp, s)
+
+                val dac = Direction.rotateAntiClockwise(d)
+                advance(dac, along, r, perp, s)
+            }
+        }
+    }
+
+    private fun advance(
+        d: Direction,
+        perp: C2Slideable,
+        r: C2Route,
+        along: C2Slideable,
+        s: State<C2Route>
+    ) {
         when (d) {
-            Direction.RIGHT, Direction.DOWN -> d2.routesTo(true).forEach { (k, v) ->
-                val r2 = C2Route(r, C2Point(d1, k, d), r.cost + v)
+            Direction.RIGHT, Direction.DOWN -> perp.routesTo(true).forEach { (k, v) ->
+                val r2 = C2Route(r, C2Point(along, k, d), r.cost + v)
                 s.add(r2)
                 log.send("Added: $r2")
             }
-            Direction.LEFT, Direction.UP -> d2.routesTo(false).forEach { (k, v) ->
-                val r2 = C2Route(r, C2Point(d1, k, d), r.cost + v)
+
+            Direction.LEFT, Direction.UP -> perp.routesTo(false).forEach { (k, v) ->
+                val r2 = C2Route(r, C2Point(along, k, d), r.cost + v)
                 s.add(r2)
                 log.send("Added: $r2")
             }
