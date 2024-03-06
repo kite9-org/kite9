dca74a6ff53133bd49d1d0a9cbf50b2545504922
Rob Moffat <rob@kite9.com>
2024-03-06 08:57:27 +0000

    Fixing caching bug in font resolution

--- kite9-visualization/src/jvmMain/java/org/kite9/diagram/dom/css/CachingCSSEngine.java
+++ kite9-visualization/src/jvmMain/java/org/kite9/diagram/dom/css/CachingCSSEngine.java
@@ -1,8 +1,11 @@
 package org.kite9.diagram.dom.css;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.batik.css.engine.CSSContext;
+import org.apache.batik.css.engine.FontFaceRule;
 import org.apache.batik.css.engine.SVG12CSSEngine;
 import org.apache.batik.css.engine.StyleSheet;
 import org.apache.batik.css.engine.value.ShorthandManager;
@@ -14,8 +17,8 @@ import org.w3c.css.sac.InputSource;
 import org.w3c.dom.Document;
 
 /**
- * Provides functionality for caching loaded stylesheets
- * 
+ * Provides functionality for caching loaded stylesheets.
+ *
  * @author robmoffat
  *
  */
@@ -38,13 +41,20 @@ public class CachingCSSEngine extends SVG12CSSEngine {
 			for (int i = 0; i < existing.getSize(); i++) {
 				ss.append(existing.getRule(i));
 			}
+
+			List<FontFaceRule> existingRules = cache.getFontFaceRules(cssUri);
+			this.fontFaces.addAll(existingRules);
+
 		} else {
 			if (cssUri == null) {
 				is.setURI(uri.toString());
 			}
-			
+
+			int oldFonts = this.fontFaces.size();
 			super.parseStyleSheet(ss, is, uri);
+			int newFonts = this.fontFaces.size();
 			cache.set(cssUri, Cache.STYLESHEET, ss);
+			cache.set(cssUri, Cache.FONT_FACE_RULES, new ArrayList<>(this.fontFaces.subList(oldFonts, newFonts)));
 		}
 	}
 	
