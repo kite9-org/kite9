ff0b8d98f2070200f061b527382f2bda60297eb2
Rob Moffat <rob@kite9.com>
2026-03-28 17:24:57 +0000

    Only one connection test failing

--- kite9-visualization/src/commonMain/kotlin/org/kite9/diagram/visualization/compaction2/C2CompactionImpl.kt
+++ kite9-visualization/src/commonMain/kotlin/org/kite9/diagram/visualization/compaction2/C2CompactionImpl.kt
@@ -172,31 +172,31 @@ class C2CompactionImpl(private val diagram: Diagram) : C2Compaction {
 
 
     override fun copyNeighbourMap(from: C2Slideable?, to: C2Slideable?) {
-//        if ((from != null) && (to != null)) {
-//            val toIsOrbit = to.getOrbitAnchors().isNotEmpty()
-//            val fromIsOrbit = from.getOrbitAnchors().isNotEmpty()
-//
-//            val toGroup = mutableSetOf<C2Slideable>()
-//
-//            // add "to" to any neighbour set containing from
-//            this.neighbourDetails.entries.forEach { (k, ss) ->
-//                val addTo = ss.filter { it.contains(from) }
-//                ss.removeAll(addTo.toSet() )
-//                val afterAdd = addTo.map { it + to }
-//                ss.addAll(afterAdd)
-//                if (addTo.isNotEmpty()) {
-//                    toGroup.add(k)
-//                }
-//            }
-//
-//            if (toIsOrbit && toGroup.isNotEmpty()) {
-//                val first = toGroup.first()
-//                val rest = toGroup - first
-//                rest.forEach { r ->
-//                    addNeighbour(to, first, r)
-//                }
-//            }
-//        }
+        if ((from != null) && (to != null)) {
+            val toIsOrbit = to.getOrbitAnchors().isNotEmpty()
+            val fromIsOrbit = from.getOrbitAnchors().isNotEmpty()
+
+            val toGroup = mutableSetOf<C2Slideable>()
+
+            // add "to" to any neighbour set containing from
+            this.neighbourDetails.entries.forEach { (k, ss) ->
+                val addTo = ss.filter { it.contains(from) }
+                ss.removeAll(addTo.toSet() )
+                val afterAdd = addTo.map { it + to }
+                ss.addAll(afterAdd)
+                if (addTo.isNotEmpty()) {
+                    toGroup.add(k)
+                }
+            }
+
+            if (toIsOrbit && toGroup.isNotEmpty()) {
+                val first = toGroup.first()
+                val rest = toGroup - first
+                rest.forEach { r ->
+                    addNeighbour(to, first, r)
+                }
+            }
+        }
     }
 
 }
